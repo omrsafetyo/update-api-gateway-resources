610ac17201ab81d6f645960cfa48f31ecb310f0f
Nathan Kulas <nathan.kulas@tylertech.com>
2022-07-29 16:27:07 -0400

    Adding initial cloud formation template

--- build-zips.ps1
+++ build-zips.ps1
@@ -1,4 +1,4 @@
-Get-ChildItem tyler-dr* -Directory | ForEach-Object {
+Get-ChildItem -Directory | ForEach-Object {
     $ZipName = "{0}.zip" -f $_.Name
     Get-ChildItem -Path $_.FullName | Compress-Archive -DestinationPath $ZipName -force
 }
